51c9fa0163a10bb5a7815662bdf37f0c66b0dde6
t-sauer <t-sauer@outlook.de>
2016-11-20 01:16:37 +0100

    Added some structure to the code

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,38 @@
 'use strict';
-import * as vscode from 'vscode';
 import * as os from 'os';
 import * as fs from 'fs';
 import * as path from 'path';
 import * as crypto from 'crypto';
 
+import {
+    commands,
+    Disposable,
+    ExtensionContext,
+    TextDocument,
+    Uri,
+    ViewColumn,
+    window,
+    workspace
+} from 'vscode';
+
 import * as babel from 'babel-core';
 
-// import * as babel from 'babel-core';
-
 const tmpFolder = os.tmpdir();
+let repl: Repl;
 
-export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.newRepl', () => {
+export function activate(context: ExtensionContext) {
+    let disposable = commands.registerCommand('extension.newRepl', () => {
 
-        new Repl();
+        repl = new Repl();
     });
 
     context.subscriptions.push(disposable);
 }
+
+export function deactivate() {
+    repl.shutdown();
+}
+
 
 const babelOptions: babel.TransformOptions = {
     presets: [path.join(__dirname, '../../node_modules/babel-preset-latest')]
@@ -26,51 +40,81 @@ const babelOptions: babel.TransformOptions = {
 
 class Repl {
 
-    inputDocument: vscode.TextDocument;
-
-    outputDocument: vscode.TextDocument;
+    changeEventDisposable: Disposable;
 
     constructor() {
-        const random = crypto.randomBytes(4).readUInt32LE(0);
-        
-        const inputFilePath = path.join(tmpFolder, 'vscode-babel-repl', `input_${random}.js`);
-        const outputFilePath = path.join(tmpFolder, 'vscode-babel-repl', `output_${random}.js`);
-
-        if (!fs.existsSync(path.join(tmpFolder, 'vscode-babel-repl'))) {
-            fs.mkdirSync(path.join(tmpFolder, 'vscode-babel-repl'));
-        }
-        
-        fs.closeSync(fs.openSync(inputFilePath, 'w'));
-        fs.closeSync(fs.openSync(outputFilePath, 'w'));
-
-        const inputUri = vscode.Uri.parse(`file://${inputFilePath}`);
-        const outputUri = vscode.Uri.parse(`file://${outputFilePath}`);
-
-        vscode.workspace.openTextDocument(inputUri).then((inputDocument) => {
-            this.inputDocument = inputDocument;
-
-            vscode.window.showTextDocument(inputDocument, vscode.ViewColumn.One);
-
-            const changeEvent = vscode.workspace.onDidChangeTextDocument((event) => {
-                if (event.document !== inputDocument) {
-                    return;
-                }
-
-                const code = event.document.getText();
-                const transformedCode = babel.transform(code, babelOptions);
-
-                fs.writeFileSync(outputFilePath, transformedCode.code);
-            });
-        });
-        vscode.workspace.openTextDocument(outputUri).then((outputDocument) => {
-            this.outputDocument = outputDocument;
-
-            vscode.window.showTextDocument(outputDocument, vscode.ViewColumn.Two, true);
-        });
+        this.init();
     }
 
-}
+    async init() {
+        const basePath = this.generateBasePath();
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+        const inputFilePath = path.join(basePath, `input.js`);
+        const outputFilePath = path.join(basePath, `output.js`);
+
+        this.generateFiles(inputFilePath, outputFilePath);
+
+        const {
+            inputTextDocument,
+            outputTextDocument
+        } = await this.openTextDocuments(inputFilePath, outputFilePath);
+
+        const {
+            inputEditor,
+            outputEditor
+        } = await this.showTextDocuments(inputTextDocument, outputTextDocument);
+
+        const changeEvent = workspace.onDidChangeTextDocument((event) => {
+            if (event.document !== inputTextDocument) {
+                return;
+            }
+
+            const code = event.document.getText();
+            const transformedCode = babel.transform(code, babelOptions);
+
+            fs.writeFileSync(outputFilePath, transformedCode.code);
+        });
+
+        this.changeEventDisposable = changeEvent;
+    }
+
+    private async openTextDocuments(inputFilePath: string, outputFilePath: string) {
+        const inputUri = Uri.parse(`file://${inputFilePath}`);
+        const outputUri = Uri.parse(`file://${outputFilePath}`);
+
+        const inputTextDocument = await workspace.openTextDocument(inputUri);
+        const outputTextDocument = await workspace.openTextDocument(outputUri);
+
+        return { inputTextDocument, outputTextDocument };
+    }
+
+    private showTextDocuments(inputTextDocument: TextDocument, outputTextDocument: TextDocument) {
+        const inputPromise = window.showTextDocument(inputTextDocument, ViewColumn.One);
+        const outputPromise = window.showTextDocument(outputTextDocument, ViewColumn.Two);
+        const promises = [inputPromise, outputPromise];
+
+        return Promise.all(promises)
+            .then(([inputEditor, outputEditor]) => ({ inputEditor, outputEditor }));
+    }
+
+    private generateBasePath(): string {
+        const random = crypto.randomBytes(4).readUInt32LE(0).toString();
+        const basePath = path.join(tmpFolder, 'vscode-babel-repl', random);
+
+        if (!fs.existsSync(basePath)) {
+            fs.mkdirSync(basePath);
+        }
+
+        return basePath;
+    }
+
+    private generateFiles(inputFilePath: string, outputFilePath: string) {
+        fs.closeSync(fs.openSync(inputFilePath, 'w'));
+        fs.closeSync(fs.openSync(outputFilePath, 'w'));
+    }
+
+    shutdown() {
+        this.changeEventDisposable.dispose();
+    }
+
 }
